5b56148237c18421864277477b0816d160f5cbb3
AZ <thecoolabbas@gmail.com>
2019-07-02 23:52:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pytz # timezone
 import requests
 import os
 
-#abbas
+
 
 app = Flask(__name__)
 
@@ -27,7 +27,7 @@ def add_numbers_post():
 	  	return render_template('add_numbers.html')
 	  elif request.method == 'POST':
   	      print(request.form['text'].split())
-  	      total = 0
+  	      total = 1
   	      try:
   	      	for str_num in request.form['text'].split():
   	      		total += int(str_num)
